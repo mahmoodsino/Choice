1e3862eaed171ded430cbc9c3d5729433f8083f6
mahmood2122 <mahmoodsino40@gmail.com>
2022-09-06 17:18:24 +0300

    fix

--- components/layouts/home/sections/Categories.tsx
+++ components/layouts/home/sections/Categories.tsx
@@ -6,14 +6,14 @@ const Categorie= ["Air Filter","Oil Filter","Chemicals","Motor Oil","Lubricants"
 
 const Categories = () => {
   return (
-    <div key={v4()} className='border  mt-10 w-[90%]'>
+    <div  className='border  mt-10 w-[90%]'>
       <div className='bg-yellow-950 flex   justify-between m-0.5 px-2 py-2'>
         <span className='font-bold'>CATEGORIES</span>
         <BurgerIcon className='text-black w-6' />
       </div>
       {Categorie.map(item => {
         return(
-            <span className='block px-2 py-3 text-xs border-b text-gray-1200'>{item}</span>
+            <span key={v4()} className='block px-2 py-3 text-xs border-b text-gray-1200'>{item}</span>
         )
       })}
     </div>
